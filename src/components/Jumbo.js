355d112d1f80e9d875aa475d3381981a99c89417
sue <sue@MacBook-Pro-de-sue.local>
2020-12-06 18:54:22 -0600

    correct compiling error

---
+++ src/components/Jumbo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyledJumbo } from '../styles/components';
+
+const Jumbo = () => {
+    return (
+        <StyledJumbo>
+            <div>
+                <h2>
+                    ¡Decora los corazones de la gente que amas, con arreglos de globos!
+                </h2>
+                <small>Bouquetes como tu los imagines</small>
+            </div>
+        </StyledJumbo>
+    );
+};
+export default Jumbo;
